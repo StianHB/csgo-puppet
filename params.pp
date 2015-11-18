4640e64a3f186517b762350f6d197d1f02bd46e3
Syoc Reich <syoc@d1stkfactory>
2015-11-18 15:34:20 +0000

    cleanup

--- params.pp
+++
@@ -1,8 +0,0 @@
-class steamcmd::params {
-	$install_directory 	= '/home/steam/steamcmd'
-	$url 			= 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz'
-	$login			= 'anonymous'
-	$password		= undef
-	$app_id			= '740'
-	$config_path		= "${install_directory}/conf"
-}
